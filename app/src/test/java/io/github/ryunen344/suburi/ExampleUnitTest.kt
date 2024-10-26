87783f029ef3d4edf26e6572725c48ff750d5b69
RyuNen344 <s1100633@outlook.com>
2024-10-26 12:49:05 +0900

    chore: setup optins

--- app/src/test/java/io/github/ryunen344/suburi/ExampleUnitTest.kt
+++ app/src/test/java/io/github/ryunen344/suburi/ExampleUnitTest.kt
@@ -1,16 +1,28 @@
 package io.github.ryunen344.suburi
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.github.ryunen344.suburi.test.rules.TimberTestRule
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import timber.log.Timber
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
+
+    @get:Rule
+    val timberRule = TimberTestRule()
+
     @Test
     fun addition_isCorrect() {
+        Timber.d("start")
         assertEquals(4, 2 + 2)
+        Timber.d("end")
     }
 }
