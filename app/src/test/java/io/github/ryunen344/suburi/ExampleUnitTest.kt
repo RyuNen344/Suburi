a8eb580bac4464698c92bf87ef16b5df7708849e
RyuNen344 <s1100633@outlook.com>
2024-12-27 09:55:44 +0900

    chore: update resource settings (#52)

--- app/src/test/java/io/github/ryunen344/suburi/ExampleUnitTest.kt
+++ app/src/test/java/io/github/ryunen344/suburi/ExampleUnitTest.kt
@@ -1,7 +1,12 @@
 package io.github.ryunen344.suburi
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import io.github.ryunen344.suburi.test.rules.TimberTestRule
+import kotlinx.coroutines.debug.DebugProbes
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -24,5 +29,26 @@ class ExampleUnitTest {
         Timber.d("start")
         assertEquals(4, 2 + 2)
         Timber.d("end")
+    }
+
+    @Test
+    fun useDebugProbes() = runTest {
+        DebugProbes.withDebugProbes {
+            DebugProbes.printScope(this)
+            val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+            backgroundScope.launch(testDispatcher) {
+                // Context of the app under test.
+                val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+                assertEquals("io.github.ryunen344.suburi", appContext.packageName)
+                DebugProbes.printScope(this)
+            }
+            DebugProbes.printScope(backgroundScope)
+            val coroutineInfo = DebugProbes.dumpCoroutinesInfo()
+            if (coroutineInfo.isNotEmpty()) {
+                coroutineInfo.forEach(::println)
+            } else {
+                println("coroutineInfo is empty")
+            }
+        }
     }
 }
