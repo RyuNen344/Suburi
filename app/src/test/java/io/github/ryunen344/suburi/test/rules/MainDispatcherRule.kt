1f809a79fdce69b867f946c96955456b301c5c36
RyuNen344 <s1100633@outlook.com>
2025-04-27 01:24:26 +0900

    chore: add MainDispatcherRule for testing with coroutines

---
+++ app/src/test/java/io/github/ryunen344/suburi/test/rules/MainDispatcherRule.kt
@@ -0,0 +1,25 @@
+package io.github.ryunen344.suburi.test.rules
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * JUnit4 TestWatcher for replacing Main Dispatcher
+ * https://developer.android.com/kotlin/coroutines/test#setting-main-dispatcher
+ */
+class MainDispatcherRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
