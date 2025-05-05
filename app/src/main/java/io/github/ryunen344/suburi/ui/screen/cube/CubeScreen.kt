930a955ea15cb9b1fba57ed32efe3b387078438f
RyuNen344 <s1100633@outlook.com>
2025-05-06 03:39:49 +0900

    chore: enhance CubeScreen with animated cube rotation and dynamic slider control

--- app/src/main/java/io/github/ryunen344/suburi/ui/screen/cube/CubeScreen.kt
+++ app/src/main/java/io/github/ryunen344/suburi/ui/screen/cube/CubeScreen.kt
@@ -1,9 +1,8 @@
 package io.github.ryunen344.suburi.ui.screen.cube
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -15,8 +14,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -24,6 +26,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Matrix
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
@@ -46,6 +49,31 @@ private val IdentityVertices = arrayOf(
     intArrayOf(-1, -1, 1),
 )
 
+// index of vertices
+private val Lines = arrayOf(
+    // rear
+    intArrayOf(0, 1, Color.Red.toArgb()),
+    intArrayOf(1, 2, Color.Red.toArgb()),
+    intArrayOf(2, 3, Color.Red.toArgb()),
+    intArrayOf(3, 0, Color.Red.toArgb()),
+    // front
+    intArrayOf(4, 5, Color.Blue.toArgb()),
+    intArrayOf(5, 6, Color.Blue.toArgb()),
+    intArrayOf(6, 7, Color.Blue.toArgb()),
+    intArrayOf(7, 4, Color.Blue.toArgb()),
+    // depth
+    intArrayOf(0, 4, Color.Green.toArgb()),
+    intArrayOf(1, 5, Color.Green.toArgb()),
+    intArrayOf(2, 6, Color.Green.toArgb()),
+    intArrayOf(3, 7, Color.Green.toArgb()),
+)
+
+private val Colors = arrayOf(
+    Color.Red,
+    Color.Green,
+    Color.Blue,
+)
+
 @Composable
 internal fun CubeScreen() {
     Scaffold(
@@ -57,35 +85,50 @@ internal fun CubeScreen() {
                 .padding(padding)
                 .padding(16.dp),
         ) {
-            val sliderState = remember { SliderState(valueRange = 1f..100f) }
+            var valueChangeFinished by remember { mutableStateOf(false) }
+            val durationMillisSlider = remember {
+                SliderState(
+                    value = 10000f,
+                    steps = 9,
+                    onValueChangeFinished = { valueChangeFinished = true },
+                    valueRange = 1000f..10000f,
+                )
+            }
+
             Slider(
-                state = sliderState,
-                modifier = Modifier.fillMaxWidth(),
+                state = durationMillisSlider,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
             )
+
             BoxWithConstraints(
                 modifier = Modifier
                     .padding(16.dp)
                     .fillMaxSize(),
                 contentAlignment = Alignment.Center,
             ) {
-                val infiniteTransition = rememberInfiniteTransition(label = "cube angle")
-                val angle by infiniteTransition.animateFloat(
-                    initialValue = 0f,
-                    targetValue = 360f,
-                    animationSpec = infiniteRepeatable<Float>(
-                        animation = tween(
-                            durationMillis = 4500,
-                            easing = LinearEasing,
+                val angle = remember { Animatable(0f) }
+                LaunchedEffect(valueChangeFinished) {
+                    valueChangeFinished = false
+                    val current = angle.value
+                    angle.animateTo(
+                        targetValue = 720f + current,
+                        animationSpec = infiniteRepeatable(
+                            animation = tween(
+                                durationMillis = durationMillisSlider.value.toInt(),
+                                easing = LinearEasing,
+                            ),
                         ),
-                    ),
-                )
-                val size = min(this.maxWidth, this.maxHeight) / 2f
+                    )
+                }
 
                 CachedCanvas(
-                    modifier = Modifier.size(size),
+                    modifier = Modifier.size(min(this.maxWidth, this.maxHeight) / 2f),
                 ) {
                     val width = (this.size.width / 4f).absoluteValue
-                    val vertices = vertices(width, angle)
+                    val vertices = vertices(width, angle.value)
+                    val lines = lines(vertices)
 
                     onDrawBehind {
                         translate(
@@ -98,6 +141,15 @@ internal fun CubeScreen() {
                                 color = Color.Red,
                                 strokeWidth = STROKE_POINT,
                             )
+
+                            lines.forEach { (start, end, color) ->
+                                drawLine(
+                                    color = color,
+                                    start = start,
+                                    end = end,
+                                    strokeWidth = STROKE_POINT,
+                                )
+                            }
                         }
                     }
                 }
@@ -113,8 +165,8 @@ private fun vertices(
     val matrix = RotationMatrix.apply {
         reset()
         rotateX(angle)
-        rotateY(angle)
-        rotateZ(angle)
+        rotateY(angle / 2)
+        rotateZ(-angle)
     }
 
     return IdentityVertices.map { vertex ->
@@ -128,6 +180,15 @@ private fun vertices(
         val screenY = rotated[1] * scale
 
         Offset(screenX, screenY)
+    }
+}
+
+private fun lines(
+    vertices: List<Offset>,
+): List<Triple<Offset, Offset, Color>> {
+    require(vertices.size == IdentityVertices.size) { "vertices size must be ${IdentityVertices.size}" }
+    return Lines.mapIndexed { index, line ->
+        Triple(vertices[line[0]], vertices[line[1]], Colors[index / 4])
     }
 }
 
